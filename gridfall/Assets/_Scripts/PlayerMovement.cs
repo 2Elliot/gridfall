fdc7fe4c1a183d54fbde4e6850cdb73334127743
Elliot <ejdalessa@gmail.com>
2023-06-06 09:29:45 -0700

    Fixed movement

--- gridfall/Assets/_Scripts/PlayerMovement.cs
+++ gridfall/Assets/_Scripts/PlayerMovement.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,8 +9,10 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField] private InputHandler _inputHandler;
 
 	[Header("Movement Settings"), Space(5)]
-	[SerializeField] private float _moveDistance = 1f;
+	[SerializeField] private int _moveDistance = 1;
 	[SerializeField] [Tooltip("In Seconds")] private float _moveTime = 1f;
+	[SerializeField] [Tooltip("In Seconds")] private float _rotationTime = 1f;
+
 
 	private bool _isMoving = false;
 
@@ -40,38 +41,58 @@ public class PlayerMovement : MonoBehaviour
 	}
 
 	private IEnumerator Move(Vector2 direction) {
+		Vector3 previousPosition = transform.position;
+		Vector3 targetDirection = new Vector3(direction.x, 0, direction.y);
+
 		// Fix direction to 4 directions
-		if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {
-			direction.y = 0;
-			direction.x = Mathf.Ceil(direction.x);
+		if (Mathf.Abs(targetDirection.x) > Mathf.Abs(targetDirection.z)) {
+			targetDirection.z = 0;
+			targetDirection.x = Mathf.Ceil(direction.x);
 		} else {
-			direction.x = 0;
-			direction.y = Mathf.Ceil(direction.y);
+			targetDirection.x = 0;
+			targetDirection.z = Mathf.Ceil(direction.y);
 		}
 
-		// Raycast check for object in the way
-		Vector3 targetLocation = transform.position + (Quaternion.Euler(0, transform.rotation.y, 0) * new Vector3(direction.x, 0, direction.y)) * _moveDistance; // Fixes targetLocation from previous rotation
-		float timer = 0f;
+		// Raycast check for object in the way	
+
+		// Finds target location based off y rotation
+		Vector3 targetLocation = transform.position + (Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y)) * _moveDistance;
 		
+		float timer = 0f;	
 		while (Vector3.Distance(transform.position, targetLocation) > 0.05f) {
-			transform.position = Vector3.Lerp(transform.position, targetLocation, timer / _moveTime);
+			transform.position = Vector3.Lerp(previousPosition, targetLocation, timer / _moveTime);
 			timer += Time.deltaTime;
 			yield return null;
 		}
 		transform.position = targetLocation;
+		FixPosition();
 		_isMoving = false;
 	}
 
 	private IEnumerator Rotate(int direction) {
-		// Raycast check for object in the way
+		Vector3 previousRotation = transform.eulerAngles;
+
+		// Gets target rotation
 		Vector3 targetRotation = transform.eulerAngles + new Vector3(0, 90 * direction, 0);
+		Vector3 fixedTargetRotation = targetRotation;
+
+		// Fixes rotation for while check to work
+		if (targetRotation.y < 0) fixedTargetRotation.y += 360;
+		if (targetRotation.y >= 360) fixedTargetRotation.y -= 360;
+
 		float timer = 0f;
-		while (Vector3.Distance(transform.eulerAngles, targetRotation) > 0.05f) {
-			transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetRotation, timer / _moveTime);
+		while (Vector3.Distance(transform.eulerAngles, fixedTargetRotation) > 0.05f) {
+			transform.eulerAngles = Vector3.Lerp(previousRotation, targetRotation, timer / _rotationTime);
 			timer += Time.deltaTime;
 			yield return null;
 		}
 		transform.eulerAngles = targetRotation;
+		FixPosition();
 		_isMoving = false;
+	}
+
+	private void FixPosition() {
+		// Rounds position to nearest int
+		transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), Mathf.Round(transform.position.z));
 	}
 }
